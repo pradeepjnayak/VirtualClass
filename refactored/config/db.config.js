755526288a0f9095233f13d72b7beaca08ef8962
Pradeep <pradeep@karthavya.com>
2020-10-15 23:27:50 +0530

    * final

--- refactored/config/db.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DB_URL = 'postgres://me:password@localhost:5432/virtual_class'
-  };
--- refactored/models/db.js
+++
@@ -1,9 +0,0 @@
-const { model } = require("mongoose");
-const dbConfig = require("../config/db.config");
-const Pool = require('pg').Pool
-
-const connection = new Pool({
-  connectionString: connectionString,
-})
-
-module.exports = connection;
